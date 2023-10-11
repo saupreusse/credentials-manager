5a2eeb2624ff5c4dc17ca512d10c6cdb801514d2
Julian René <julian_rene_@hotmail.com>
2023-10-11 17:09:42 +0200

    Delete credentials_manager directory

--- credentials_manager/server/tests.py
+++
@@ -1,9 +0,0 @@
-import rotator
-
-label = "webappcr"
-newpassword = "nRhYPMhk+k-^qIOz"
-
-rotator.finishRotation(label,newpassword)
-#rotator.testConnection(label)
-#rotator.setPassword(label, newpassword)
-#rotator.testConnection(label, newpassword)
